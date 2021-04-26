24923b2f21dc5ee4516bc5c9effef3fc211161a6
a376148946 <34079036+a376148946@users.noreply.github.com>
2021-04-26 20:56:57 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,25 @@
+import asyncio
+import base64
+import os
+import random
+import sqlite3
+import math
+from datetime import datetime, timedelta
+from io import BytesIO
+from PIL import Image
+from hoshino import Service, priv
+from hoshino.modules.priconne import _pcr_data
+from hoshino.modules.priconne import chara
+from hoshino.typing import CQEvent
+from hoshino.util import DailyNumberLimiter
+import copy
+import json
+
+sv = Service('pcr-duel', enable_on_default=True)
+from .CECounter import *
+from .ScoreCounter import *
+from .DuelCounter import *
+#贵族决斗主体
+from .zhuti import *
+#战斗力系统调用
+from .comateffectiveness import *
